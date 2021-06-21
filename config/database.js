bf60b9c4a49fcbaaa9c284c418465dfe84ba80e1
Batuhan Yılmaz <a.batuhan.yilmaz@icloud.com>
2021-06-21 20:30:14 +0300

    updated

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
-module.exports = ({ env }) => ({
+/* module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
@@ -10,6 +10,30 @@ module.exports = ({ env }) => ({
       options: {
         useNullAsDefault: true,
       },
+    },
+  },
+}); */
+
+const parse = require("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL);
+
+module.exports = () => ({
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "bookshelf",
+      settings: {
+        client: "postgres",
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        username: config.user,
+        password: config.password,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
+      options: {},
     },
   },
 });
